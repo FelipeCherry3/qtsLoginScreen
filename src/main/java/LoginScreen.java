736877d4dc23c1c57e14ed0147131c654106fb78
FelipeCherry3 <felipeelias609@hotmail.com>
2024-04-30 21:52:42 -0300

    javadoc

--- src/main/java/LoginScreen.java
+++ src/main/java/LoginScreen.java
@@ -1,15 +1,28 @@
 import java.util.HashMap;
 import java.util.Map;
+/**
+Classe que realiza um serviço de autenticação simples de Login e Senha
 
+ **/
 public class LoginScreen {
-    HashMap<String,String> userDatabase = new HashMap<String,String>();
+    Map<String,String> userDatabase;
 
+    /**
+     * Construtor Inicializa e Adiciona Valores em Nosso HashMap
+     */
     public LoginScreen() {
+        userDatabase = new HashMap<String,String>();
         userDatabase.put("felipe","senha123");
         userDatabase.put("jose","senha");
         userDatabase.put("caio","senha123");
     }
 
+    /**
+     *
+     * @param login Input login
+     * @param senha Input senha
+     * @return {@code true} autenticação foi um sucesso, {@code false} autenticação falhou
+     */
     public boolean login(String login, String senha) {
         for(Map.Entry<String,String> entry : userDatabase.entrySet()){
             String loginHash = entry.getKey();
